2630dbf1f1c0805231e9db017bd7e420493de7ab
aliaksandrtseliuk <52308758+aliaksandrtseliuk@users.noreply.github.com>
2020-03-03 10:03:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    let onesArray = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    let betArray = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let tensArray = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    
+    function concat(num) {
+        return (num === 0) ? 'zero' : get100(num)
+    }
+
+    function get100(num) {
+        return (num > 99) ? onesArray[Math.floor(num / 100)] + " hundred " + get10(num % 100) : get10(num)
+    }
+
+    function get10(num) {
+        return (num < 10) ? onesArray[num] :
+        (num >= 10 && num < 20) ? betArray[num - 10] : tensArray[Math.floor(num / 10)] + " " + onesArray[num % 10]
+    }
+
+    return concat(number).trim()
 }
